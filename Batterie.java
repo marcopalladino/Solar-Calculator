6e9b61e99c11b9500469d710c77e0d6bf8b0f8f8
KleineTeufel <76700422+KleineTeufel@users.noreply.github.com>
2021-01-10 18:53:52 +0100

    Update Batterie.java

--- Batterie.java
+++ Batterie.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Lavoro del gruppo Mhackeroni
+ * MAroni Calvi, Scotto, Pettigiani
+ * III M
  */
 package vorii;
 import java.util.Scanner;
